7881496295748582933ac6edfe12db8ea317fd44
Kevin Liu <liukproductivity@gmail.com>
2023-12-28 11:04:05 -0500

    faster solution for topK

--- arrays_hashing/top_k_freq_elements.py
+++ arrays_hashing/top_k_freq_elements.py
@@ -1,14 +1,29 @@
+# def topKFrequent(nums, k):
+#     table = {}
+#     top = []
+#     for i in range(len(nums)):
+#         if nums[i] in table:
+#             table[nums[i]] += 1
+#         else:
+#             table[nums[i]] = 1
+#     for j in range(k):
+#         top.append(sorted(table.items(), key=lambda item: item[1], reverse=True)[j][0])
+#     return top
+
 def topKFrequent(nums, k):
     table = {}
+    freq = [[] for i in range(len(nums) + 1)]
     top = []
-    for i in range(len(nums)):
-        if nums[i] in table:
-            table[nums[i]] += 1
-        else:
-            table[nums[i]] = 1
-    for j in range(k):
-        top.append(sorted(table.items(), key=lambda item: item[1], reverse=True)[j][0])
-    return top
+
+    for n in nums:
+        table[n] = 1 + table.get(n, 0)
+    for n, c in table.items():
+        freq[c].append(n)
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            top.append(n)
+            if len(top) == k:
+                return top
 
 if __name__ == '__main__':
     case1 = ([1,1,1,2,2,3], 2)
