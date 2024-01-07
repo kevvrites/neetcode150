113c0598607248409ff97d8d65de8720d9463a30
Kevin Liu <liukproductivity@gmail.com>
2024-01-07 12:22:24 -0500

    added test cases

--- arrays_hashing/valid_anagram.py
+++ arrays_hashing/valid_anagram.py
@@ -1,2 +1,13 @@
 def isAnagram(s, t):
     return sorted(s) == sorted(t)
+
+if __name__ == '__main__':
+    case1 = ['anagram', 'nagaram']
+    case2 = ['rat', 'car']
+    cases = [case1, case2]
+
+    expected = [True, False]
+    
+    for i, exp in enumerate(expected):
+        assert isAnagram(cases[i][0], cases[i][1]) == expected[i]
+    print('ok')
