b7a614a630f28c6a1239096a0d1069178203e9df
Kevin Liu <liukproductivity@gmail.com>
2024-01-07 12:34:11 -0500

    completed valid sudoku

---
+++ arrays_hashing/valid_sudoku.py
@@ -0,0 +1,46 @@
+import collections
+
+def isValidSudoku(board):
+    cols = collections.defaultdict(set)
+    rows = collections.defaultdict(set)
+    boxes = collections.defaultdict(set)
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == '.':
+                continue
+            if (board[r][c] in rows[r] or
+                board[r][c] in cols[c] or
+                board[r][c] in boxes[(r // 3, c // 3)]):
+                return False
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            boxes[r // 3, c // 3].add(board[r][c])
+    print(boxes)
+    return True
+
+if __name__ == '__main__':
+    case1 = [["5","3",".",".","7",".",".",".","."]
+            ,["6",".",".","1","9","5",".",".","."]
+            ,[".","9","8",".",".",".",".","6","."]
+            ,["8",".",".",".","6",".",".",".","3"]
+            ,["4",".",".","8",".","3",".",".","1"]
+            ,["7",".",".",".","2",".",".",".","6"]
+            ,[".","6",".",".",".",".","2","8","."]
+            ,[".",".",".","4","1","9",".",".","5"]
+            ,[".",".",".",".","8",".",".","7","9"]]
+    case2 = [["8","3",".",".","7",".",".",".","."]
+            ,["6",".",".","1","9","5",".",".","."]
+            ,[".","9","8",".",".",".",".","6","."]
+            ,["8",".",".",".","6",".",".",".","3"]
+            ,["4",".",".","8",".","3",".",".","1"]
+            ,["7",".",".",".","2",".",".",".","6"]
+            ,[".","6",".",".",".",".","2","8","."]
+            ,[".",".",".","4","1","9",".",".","5"]
+            ,[".",".",".",".","8",".",".","7","9"]]
+    cases = [case1, case2]
+
+    expected = [True, False]
+    
+    for i, exp in enumerate(expected):
+        assert isValidSudoku(cases[i]) == expected[i]
+    print('ok')
