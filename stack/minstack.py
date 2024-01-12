c61d36e3d259e11c7fcef073d0d830af892ca033
Kevin Liu <liukproductivity@gmail.com>
2024-01-12 12:52:02 -0500

    completed minstack

---
+++ stack/minstack.py
@@ -0,0 +1,30 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+    
+    def pop(self):
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minStack[-1]
+
+if __name__ == "__main__":
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    assert minStack.getMin() == -3
+    minStack.pop()
+    assert minStack.top() == 0
+    assert minStack.getMin() == -2
+    print('ok')
