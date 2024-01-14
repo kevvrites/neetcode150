09b906795bc8cc8e4a2d02039cc7265637c7aeeb
Kevin Liu <liukproductivity@gmail.com>
2024-01-14 12:32:18 -0500

    completed daily

--- daily_challenges/determine_if_two_strings_are_close.py
+++ daily_challenges/determine_if_two_strings_are_close.py
@@ -1,21 +1,24 @@
 def closeStrings(word1, word2):
     if len(word1) != len(word2):
         return False
-    word1 = sorted(word1)
-    word2 = sorted(word2)
-    diff = []
-    count = 0
-    for i in range(len(word1)):
-        letters = set()
-        if word1[i] != word2[i]:
-            letters.add(word1[i])
-            letters.add(word2[i])
-            diff.append(letters)
-    letters = set.union(*diff) if diff else []
-    if len(letters) > 3:
-        return False
-    else:
-        return True
+    freq1 = [0 for _ in range(26)]
+    freq2 = [0 for _ in range(26)]
+    for letter in word1:
+        freq1[ord(letter) - ord('a')] += 1
+    for letter in word2:
+        freq2[ord(letter) - ord('a')] += 1
+    
+    for i in range(26):
+        if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+            return False
+
+    freq1.sort()
+    freq2.sort()
+
+    for i in range(26):
+        if freq1[i] != freq2[i]:
+            return False
+    return True
 
 if __name__ == "__main__":
     case1 = ["abc", "bca"]
