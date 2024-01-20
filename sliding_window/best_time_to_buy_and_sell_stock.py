3d464c4b169712a0d0f6b6e64b27e70d11b454f5
Kevin Liu <liukproductivity@gmail.com>
2024-01-20 15:17:15 -0500

    completed max profit

--- sliding_window/best_time_to_buy_and_sell_stock.py
+++ sliding_window/best_time_to_buy_and_sell_stock.py
@@ -1,4 +1,4 @@
-def buy_and_sell(prices):
+def maxProfit(prices):
     l = 0
     r = 1
     max_profit = 0
@@ -6,15 +6,15 @@ def buy_and_sell(prices):
     if len(prices) == 1:
         return 0
     
-    while r <= len(prices):
-        if prices(l) > prices(r):
+    while r <= len(prices) - 1:
+        if prices[l] > prices[r]:
             l = r
             r = l + 1
-        if prices(r) >= prices(l):
-            profit = prices(r) - prices(l)
+        elif prices[r] >= prices[l]:
+            profit = prices[r] - prices[l]
             max_profit = max(max_profit, profit)
             r += 1
-    return max_profit 
+    return max_profit
 
 if __name__ == "__main__":
     case1 = [7,1,5,3,6,4]
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     expected = [5,0]
     
     for i, exp in enumerate(cases):
-        assert(buy_and_sell(exp) == expected[i])
+        assert(maxProfit(exp) == expected[i])
     print("ok") 
